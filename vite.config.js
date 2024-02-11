3422297ef47b3581ec38f2642409470855cdf412
Shreya DR <shreyadr09@gmail.com>
2024-02-11 23:07:58 +0530

    added build dir

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Specify custom entry point if different from default
-    rollupOptions: {
-      input: '/src/main.jsx', // Update the path accordingly
-    },
+    outDir: 'build', // Specify the output directory
   },
 })
