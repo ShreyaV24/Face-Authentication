4ceb9470b3dca451398b4ea0dcc2dcc103d99c39
Shreya DR <shreyadr09@gmail.com>
2024-02-11 22:58:48 +0530

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Specify custom entry point if different from default
+    rollupOptions: {
+      input: '/src/main.jsx', // Update the path accordingly
+    },
+  },
 })
